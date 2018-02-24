3a6430594461dc95f0cf942bfdc3cf1c8b0eb53c
Stepan Zastupov <stepan.zastupov@gmail.com>
2018-02-25 02:47:53 +0300

    MOve things around

--- mpp/src/index.js
+++ mpp/src/index.js
@@ -1,5 +1,5 @@
 // THIS IS WORK IN PROGRESS AND LOOKS UGLY AS FUCK
-const THREE = require("three")
+import * as THREE from "three"
 
 const audioCtx = new AudioContext()
 
@@ -13,12 +13,18 @@ const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
 renderer.setSize(canvas.width, canvas.height)
 renderer.setClearColor(0x110011, 1)
 
-// Scene
-const scene = new THREE.Scene()
-
 // Constants
 const LINE_COUNT = 100
 const CAMERA_Y = 8
+
+// Analyzer and fft buffers
+const analyser = audioCtx.createAnalyser()
+analyser.fftSize = 256
+const bufferLength = analyser.frequencyBinCount
+const dataArrays = []
+for (let i = 0; i < LINE_COUNT; i++) {
+  dataArrays.push(new Uint8Array(bufferLength))
+}
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -30,63 +36,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(0, CAMERA_Y, 100)
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
-// Analyzer and fft buffers
-const analyser = audioCtx.createAnalyser()
-analyser.fftSize = 256
-const bufferLength = analyser.frequencyBinCount
-const dataArrays = []
-for (let i = 0; i < LINE_COUNT; i++) {
-  dataArrays.push(new Uint8Array(bufferLength))
-}
-
-// Geometry
-let lines
-
-function draw() {
-  dataArrays.unshift(dataArrays.pop())
-  analyser.getByteFrequencyData(dataArrays[0])
-
-  for (let i = 0; i < LINE_COUNT; i++) {
-    const dataArray = dataArrays[i]
-    const z = i
-    const line = lines[i]
-
-    const xoff = 60
-    const width = 140
-    const height = 30
-    const geometry = line.geometry
-
-    for (let j = 0; j < bufferLength; j++) {
-      const xn = j / bufferLength
-      const yn = dataArray[j] / 256
-      geometry.vertices[j].set(xn * width - xoff, yn * height, z)
-    }
-
-    geometry.verticesNeedUpdate = true
-  }
-
-  renderer.render(scene, camera)
-
-  requestAnimationFrame(draw)
-}
-
-function init() {
-  loadTextures(scene)
-  lines = initLines(scene, bufferLength)
-
-  fetch("test.m4a")
-    .then(resp => resp.arrayBuffer())
-    .then(src => audioCtx.decodeAudioData(src))
-    .then(buffer => {
-      const source = audioCtx.createBufferSource()
-      source.buffer = buffer
-      source.connect(analyser).connect(audioCtx.destination)
-      source.start()
-    })
-
-  draw()
-}
-
+// Scene
 function initLines(scene, bufferLength) {
   const material = new THREE.LineBasicMaterial({ color: 0xd854c2 })
   const lines = []
@@ -119,6 +69,54 @@ function loadTextures(scene) {
   sunSprite.position.set(0, CAMERA_Y, -1)
   sunSprite.scale.set(30, 30, 1)
   scene.add(sunSprite)
+}
+
+// Geometry
+let lines
+
+function render(scene) {
+  dataArrays.unshift(dataArrays.pop())
+  analyser.getByteFrequencyData(dataArrays[0])
+
+  for (let i = 0; i < LINE_COUNT; i++) {
+    const dataArray = dataArrays[i]
+    const z = i
+    const line = lines[i]
+
+    const xoff = 60
+    const width = 140
+    const height = 30
+    const geometry = line.geometry
+
+    for (let j = 0; j < bufferLength; j++) {
+      const xn = j / bufferLength
+      const yn = dataArray[j] / 256
+      geometry.vertices[j].set(xn * width - xoff, yn * height, z)
+    }
+
+    geometry.verticesNeedUpdate = true
+  }
+
+  renderer.render(scene, camera)
+
+  requestAnimationFrame(() => render(scene))
+}
+
+function init() {
+  const scene = new THREE.Scene()
+  loadTextures(scene)
+  lines = initLines(scene, bufferLength)
+  render(scene)
+
+  fetch("test.m4a")
+    .then(resp => resp.arrayBuffer())
+    .then(src => audioCtx.decodeAudioData(src))
+    .then(buffer => {
+      const source = audioCtx.createBufferSource()
+      source.buffer = buffer
+      source.connect(analyser).connect(audioCtx.destination)
+      source.start()
+    })
 }
 
 // Buttons
